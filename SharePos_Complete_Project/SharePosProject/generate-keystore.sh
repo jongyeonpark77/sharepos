01c0ec01b1d54cce91e7b003ec6b420528581188
jongyeonpark77 <jongyeon7177@gmail.com>
2025-07-02 12:53:28 +0900

    Delete SharePos_Complete_Project/SharePosProject directory

--- SharePos_Complete_Project/SharePosProject/generate-keystore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Generate keystore for Android app signing
-keytool -genkey -v -keystore app/keystore.jks -keyalg RSA -keysize 2048 -validity 10000 -alias sharepos -storepass sharepos123 -keypass sharepos123 -dname "CN=SharePos, OU=Development, O=SharePos, L=Seoul, S=Seoul, C=KR"
-
-echo "Keystore generated successfully!" 
